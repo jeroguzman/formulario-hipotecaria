3f96a162152d1e5488ace4c69b85020257bfb1e4
leonel bustamante <49352240+leonelBusta@users.noreply.github.com>
2020-09-15 17:40:08 -0700

    slidebarMenu

--- FormularioHipotecaria/applications/home/urls.py
+++ FormularioHipotecaria/applications/home/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path("", views.HomeView.as_view()),
-    path("dashboard/", views.dashboardView.as_view()),
-    path("asesores/", views.asesoresView.as_view()),
-    path("clientes/", views.clientesView.as_view()),
+    path("dashboard/", views.dashboardView.as_view(), name='a-dashboard'),
+    path("asesores/", views.asesoresView.as_view(), name='a-asesores'),
+    path("clientes/", views.clientesView.as_view(), name='a-clientes'),
 
 ]
