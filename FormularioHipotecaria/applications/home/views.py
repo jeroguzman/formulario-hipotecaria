522050aa4998a0fb787cbc7a5ffe35bc6d1595ce
leonel bustamante <49352240+leonelBusta@users.noreply.github.com>
2020-09-17 15:59:05 -0700

    forms.form_asesor_alta
    class nuevoAsesorView(FormView):
        form_class = forms.form_asesor_alta
        template_name = 'dashboard/nuevoAsesor.html'

--- FormularioHipotecaria/applications/home/views.py
+++ FormularioHipotecaria/applications/home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
+from . import forms
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -8,12 +10,14 @@ class HomeView(TemplateView):
 class dashboardView(TemplateView):
     template_name = 'dashboard/dashboard.html'
 
-class asesoresView(TemplateView):
+class asesoresView(FormView):
+    form_class = forms.form_asesor_alta
     template_name = 'dashboard/asesores.html'
 
 class clientesView(TemplateView):
     template_name = 'dashboard/clientes.html'
 
-class nuevoAsesorView(TemplateView):
+class nuevoAsesorView(FormView):
+    form_class = forms.form_asesor_alta
     template_name = 'dashboard/nuevoAsesor.html'
 
