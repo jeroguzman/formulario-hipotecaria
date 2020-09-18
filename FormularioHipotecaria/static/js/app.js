b0d076ea6d9a8836b33de461f70daa9af0cd55d6
genesisbell <bellmon90@hotmail.com>
2020-09-18 00:27:44 -0700

    agrego validaciones a todos los formularios

--- FormularioHipotecaria/static/js/app.js
+++ FormularioHipotecaria/static/js/app.js
@@ -7,7 +7,9 @@ $('.col-lg-2').hover(function () {
     const currenth5 = $(this).find('h5');
     const arrow = $(this).find('.arrow');
 
+
     circulo.click(function () {
+
         $('.asesoria-circulo').not(circulo).removeClass("active");
         circulo.toggleClass("active");
 
@@ -16,6 +18,8 @@ $('.col-lg-2').hover(function () {
 
         $('.arrow').not(arrow).hide();
         arrow.toggle();
+
+        window.scrollTo(0, circulo.offset().top + 500);
 
     });
 });
@@ -27,7 +31,7 @@ $('.money').mask('000,000,000', {
 
 $('.celularInput').mask('(000) 000-0000');
 
-$('.rfcInput').mask('ZZZZ-00-00-00-AAA', {
+$('.rfcInput').mask('ZZZZ-00-00-00', {
     translation: {
         'Z': {
             pattern: /[A-Za-z]/,
@@ -38,25 +42,8 @@ $('.rfcInput').mask('ZZZZ-00-00-00-AAA', {
 
 //Forms
 $('.preguntas').hover(function () {
-    const btnPanel1 = $(this).find('.btnPanel1');
-    const btnPanel2 = $(this).find('.btnPanel2');
-    const btnPanel3 = $(this).find('.btnPanel3');
-    const btnPanel4 = $(this).find('.btnPanel4');
-    const btnPanel5 = $(this).find('.btnPanel5');
-    const btnPanel_1 = $(this).find('.btnPanel-1');
-    const btnPanel_2 = $(this).find('.btnPanel-2');
-    const btnPanel_3 = $(this).find('.btnPanel-3');
-    const btnPanel_4 = $(this).find('.btnPanel-4');
-
-    const panel1 = $(this).find('.panel1');
-    const panel2 = $(this).find('.panel2');
-    const panel3 = $(this).find('.panel3');
-    const panel4 = $(this).find('.panel4');
-    const panel5 = $(this).find('.panel5');
-    const panel_1 = $(this).find('.panel-1');
-    const panel_2 = $(this).find('.panel-2');
-    const panel_3 = $(this).find('.panel-3');
-    const panel_4 = $(this).find('.panel-4');
+    const btn = $(this).find('.btnNext');
+    const panel = $(this).find('.panel');
 
     const btnPanelEs1 = $(this).find('.btnPanelEs1');
     const panelEs1 = $(this).find('.panelEs1');
@@ -65,58 +52,13 @@ $('.preguntas').hover(function () {
     const btnPanelEs3 = $(this).find('.btnPanelEs3');
     const panelEs3 = $(this).find('.panelEs3');
 
-    btnPanel1.click(function () {
-        panel1.fadeIn(500);
-        $(this).fadeOut(100);
-    });
-
-    btnPanel2.click(function () {
-        panel2.fadeIn(500);
-        $(this).fadeOut(100);
-        window.scrollTo(0, panel2.offset().top);
-
-    });
-
-    btnPanel3.click(function () {
-        panel3.fadeIn(500);
-        $(this).fadeOut(100);
-        window.scrollTo(0, panel3.offset().top);
-    });
-
-    btnPanel4.click(function () {
-        panel4.fadeIn(500);
-        $(this).fadeOut(100);
-        window.scrollTo(0, panel4.offset().top);
-    });
-
-    btnPanel5.click(function () {
-        panel5.fadeIn(500);
-        $(this).fadeOut(100);
-        window.scrollTo(0, panel5.offset().top);
-    });
-
-    btnPanel_1.click(function () {
-        panel_1.fadeIn(500);
-        $(this).fadeOut(100);
-        window.scrollTo(0, panel_1.offset().top);
-    });
-
-    btnPanel_2.click(function () {
-        panel_2.fadeIn(500);
-        $(this).fadeOut(100);
-        window.scrollTo(0, panel_2.offset().top);
-    });
-
-    btnPanel_3.click(function () {
-        panel_3.fadeIn(500);
-        $(this).fadeOut(100);
-        window.scrollTo(0, panel_3.offset().top);
-    });
-
-    btnPanel_4.click(function () {
-        panel_4.fadeIn(500);
-        $(this).fadeOut(100);
-        window.scrollTo(0, panel_4.offset().top);
+    btn.each(function (index) {
+        const panelIndex = panel.eq(index);
+        $(this).click(function () {
+            panelIndex.fadeIn(500);
+            $(this).hide();
+            window.scrollTo(0, panelIndex.offset().top);
+        });
     });
 
     btnPanelEs1.click(function () {
@@ -140,15 +82,126 @@ $('.preguntas').hover(function () {
         $(this).addClass('ingreso-active');
 
         if ($(this).hasClass('no')) {
-            panelEs3.fadeOut(100);
+            panelEs3.hide();
             $('.alt').fadeIn(500)
-            window.scrollTo(0, $('.alt').offset().top);
+            window.scrollTo(0, $('.alt').offset().top - 500);
         } else {
-            $('.alt').fadeOut(100)
+            $('.alt').hide()
             panelEs3.fadeIn(500);
             window.scrollTo(0, panelEs3.offset().top);
         }
-
-
     });
+
+});
+
+//Validations
+const regexName = /^([a-zA-Z]{1,20})$/;
+$('input[name=valor]').on('keypress keydown keyup', function () {
+    if ($(this).cleanVal() < 500000 || $(this).cleanVal() > 180000000) {
+        $(this).parent().siblings('.emsg').removeClass('hidden');
+    } else {
+        $(this).parent().siblings('.emsg').addClass('hidden');
+    }
+});
+
+$('input[name=telefono]').on('keypress keydown keyup', function () {
+    if ($(this).cleanVal().length < 10) {
+        $(this).siblings('.emsg').removeClass('hidden');
+    } else {
+        $(this).siblings('.emsg').addClass('hidden');
+    }
+});
+
+$('input[name=rfc]').on('keypress keydown keyup', function () {
+    if ($(this).cleanVal().length < 10) {
+        $(this).siblings('.emsg').removeClass('hidden');
+    } else {
+        $(this).siblings('.emsg').addClass('hidden');
+    }
+});
+
+$('input[name=nombre]').on('keypress keydown keyup', function () {
+    if (!$(this).val().match(regexName)) {
+        $(this).siblings('.emsg').removeClass('hidden');
+    } else {
+        $(this).siblings('.emsg').addClass('hidden');
+    }
+});
+
+$('input[name=mensualidad]').on('keypress keydown keyup', function () {
+    if ($(this).cleanVal() < 2000 || $(this).cleanVal() > 240000) {
+        $(this).parent().siblings('.emsg').removeClass('hidden');
+    } else {
+        $(this).parent().siblings('.emsg').addClass('hidden');
+    }
+});
+
+$('input[name=montoALaFecha]').on('keypress keydown keyup', function () {
+    if ($(this).cleanVal() < 250000 || $(this).cleanVal() > 40000000) {
+        $(this).parent().siblings('.emsg').removeClass('hidden');
+    } else {
+        $(this).parent().siblings('.emsg').addClass('hidden');
+    }
+});
+
+$('input[name=anios]').on('keypress keydown keyup', function () {
+    if ($(this).val() < 5 || $(this).val() > 30) {
+        $(this).siblings('.emsg').removeClass('hidden');
+    } else {
+        $(this).siblings('.emsg').addClass('hidden');
+    }
+});
+
+$('form').submit(function (e) {
+
+    const valor = $(this).find('input[name=valor]');
+    const telefono = $(this).find('input[name=telefono]');
+    const rfc = $(this).find('input[name=rfc]');
+    const nombre = $(this).find('input[name=nombre]');
+    const banco = $(this).find('select[name=banco]');
+    const mensualidad = $(this).find('input[name=mensualidad]');
+    const montoALaFecha = $(this).find('input[name=montoALaFecha]');
+    const anios = $(this).find('input[name=anios]');
+    if (valor.cleanVal() < 500000 || valor.cleanVal() > 180000000) {
+        e.preventDefault();
+        valor.focus();
+        window.scrollTo(0, valor.offset().top - 50);
+
+    } else if (banco.length && (banco.val() === "Elija...")) {
+        e.preventDefault();
+        banco.focus();
+        banco.parent().siblings('.emsg').removeClass('hidden');
+        window.scrollTo(0, banco.offset().top - 50);
+
+    } else if (mensualidad.length && (mensualidad.cleanVal() < 2000 || mensualidad.cleanVal() > 240000)) {
+        e.preventDefault();
+        mensualidad.focus();
+        window.scrollTo(0, mensualidad.offset().top - 50)
+
+    } else if (montoALaFecha.length && (montoALaFecha.cleanVal() < 250000 || montoALaFecha > 40000000)) {
+        e.preventDefault();
+        montoALaFecha.focus();
+        window.scrollTo(0, montoALaFecha.offset().top - 50)
+
+    } else if (anios.length && (anios.val() < 5 || anios.val() > 30)) {
+        e.preventDefault();
+        anios.focus();
+        window.scrollTo(0, anios.offset().top - 50)
+
+    } else if (telefono.cleanVal().length < 10) {
+        e.preventDefault();
+        telefono.focus();
+        window.scrollTo(0, telefono.offset().top - 50);
+
+    } else if (rfc.cleanVal().length < 10) {
+        e.preventDefault();
+        rfc.focus();
+        window.scrollTo(0, rfc.offset().top - 50);
+
+    } else if (!nombre.val().match(regexName)) {
+        e.preventDefault();
+        nombre.focus();
+        window.scrollTo(0, nombre.offset().top - 50);
+    }
+
 });
