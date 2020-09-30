8291fa92f0ee58ec0fed2fa9cb8215dde5ae352e
leonel bustamante <49352240+leonelBusta@users.noreply.github.com>
2020-09-30 16:52:36 -0700

    class StaffRequiredMixin(object)

--- FormularioHipotecaria/applications/asesores/forms.py
+++ FormularioHipotecaria/applications/asesores/forms.py
@@ -3,13 +3,11 @@ from .models import asesores
 
 class form_Asesores(forms.ModelForm):
 
-    # datos generales
-    nombres = forms.CharField(widget = forms.TextInput(attrs = {'class': 'form-control' }))
-    usuario = forms.CharField(widget = forms.TextInput(attrs = {'class': 'form-control'}))
-    telefono = forms.CharField(widget = forms.TextInput(attrs = {'class': 'form-control'}))
-    email = forms.EmailField(widget = forms.TextInput(attrs = {'class': 'form-control' }))
-    nclientes = forms.CharField(widget = forms.TextInput(attrs = {'class': 'form-control'}))
-
     class Meta:
         model = asesores
-        fields =   ['nombres', 'usuario', 'telefono','email', 'nclientes']
+        fields =   ['nombres', 'usuario','email']
+        widgets = {
+            'nombres' :forms.TextInput(attrs={'class': 'form-control'}),
+            'usuario' : forms.TextInput(attrs={'class': 'form-control'}),
+            'email' : forms.TextInput(attrs={'class': 'form-control'}),
+        }
