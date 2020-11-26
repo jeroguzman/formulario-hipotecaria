db6e6a9681d031e04783426c6748b6b544ac1958
j0rbe3n <jeroguzman@gmail.com>
2020-11-25 20:23:49 -0700

    Jero/Cambios para deploy

--- FormularioHipotecaria/FormularioHipotecaria/settings/local.py
+++ FormularioHipotecaria/FormularioHipotecaria/settings/local.py
@@ -1,8 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
 ALLOWED_HOSTS = ['mshipotecaria.ddns.net', '127.0.0.1']
 
 # Database
---
+++ FormularioHipotecaria/FormularioHipotecaria/settings/prod.py
@@ -0,0 +1,33 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'msh',
+        'NAME': 'mshipotecaria2020',
+        'TEST': {
+            'NAME': 'msh_prod',
+        },
+    },
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
+
+# EMAIL SETTINGS
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'correomshipotecaria@gmail.com' 
+EMAIL_HOST_PASSWORD = 'Mshipotecaria20'
+EMAIL_PORT = 587
