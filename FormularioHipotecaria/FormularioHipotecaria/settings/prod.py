ccd0f6b51815791ac84d9c7b06726f18d0aab78a
j0rbe3n <jeroguzman@gmail.com>
2020-11-26 22:38:08 -0700

    Cambios para deploy

--- FormularioHipotecaria/FormularioHipotecaria/settings/prod.py
+++ FormularioHipotecaria/FormularioHipotecaria/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['165.22.15.142', 'localhost', '127.0.0.1']
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
