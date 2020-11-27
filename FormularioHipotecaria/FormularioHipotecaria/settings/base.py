a441732655a592e8d7b4cd0aba671f94abca08fb
oscar jonathan misael lin coronado <ojmlc93@gmail.com>
2020-11-26 18:21:42 -0700

    Misael/Configuraciones de producción

--- FormularioHipotecaria/FormularioHipotecaria/settings/base.py
+++ FormularioHipotecaria/FormularioHipotecaria/settings/base.py
@@ -40,7 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.WhiteNoiseMiddleware',
+    # 'django.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'FormularioHipotecaria.urls'
