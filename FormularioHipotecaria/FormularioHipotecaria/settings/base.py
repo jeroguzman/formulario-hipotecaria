ad84cb15f952c07d2ddd65b85ce51a594aafbe3c
j0rbe3n <jeroguzman@gmail.com>
2020-11-28 16:57:20 -0700

    Update base.py

--- FormularioHipotecaria/FormularioHipotecaria/settings/base.py
+++ FormularioHipotecaria/FormularioHipotecaria/settings/base.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'FormularioHipotecaria.urls'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATES = [
     {
