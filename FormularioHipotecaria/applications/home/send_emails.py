56c3cff812aa7256b30599d0cf065a8e57911201
oscar jonathan misael lin coronado <ojmlc93@gmail.com>
2020-12-08 12:37:04 -0700

    Misael/Nueva estructura de correos

--- FormularioHipotecaria/applications/home/send_emails.py
+++ FormularioHipotecaria/applications/home/send_emails.py
@@ -11,57 +11,55 @@ class Messenger:
         email_prom = client.promotor.email
         email_as = User.objects.get(username=client.promotor.asesor).email
         admin_subject = 'Perfilador web /{}/{}'.format(client.promotor, client.nombre)
+        admin_content = 'Un nuevo cliente se ha registrado en tu enlace de Perfilamiento de Crédito Hipotecario:'
+        template_mail = 'admin_mail.html'
 
-        admin_content = ''
-        admin_content += 'Un nuevo cliente se ha registrado en tu enlace de Perfilamiento de Crédito Hipotecario:'\
-            '\n Asesor: {}\n Promotor: {}\n'\
-            '\n Nombre Cliente: {}\n Telefono: {}\n Correo: {}\n'\
-            '\n Tipo de Credito: {}\n Inmueble Identificado: {}\n Valor del Inmueble: {}\n'.format(
-                client.promotor.asesor,
-                client.promotor,
-                client.nombre,
-                client.telefono,
-                client.email,
-                client.tramite,
-                client.inmueble_identificado,
-                client.valor_inmueble
-            )
+        if client.ingreso_mensual_co_acreditado is None:
+            client.ingreso_mensual_co_acreditado = 0.0
 
-        if client.actividad == 'Empleado':
-            admin_content += '\n Actividad: {}\n Cotiza: {}\n Esta pagando crédito Infonavit o Fovissste: {}\n '\
-                'Ingreso Mensual: {:,}\n Estado Civil: {}\n'.format(
-                client.actividad,
-                client.institucion,
-                client.pagando_credito_inmb,
-                client.ingreso_mensual,
-                client.estado_civil
-            )
-        else:
-            admin_content += '\n Actividad: {}\n Ingreso Mensual: {:,}\n Giro: {}\n Estado Civil: {}\n'.format(
-                client.actividad,
-                client.ingreso_mensual,
-                client.giro_actividad,
-                client.estado_civil
-            )
+        ctx = {
+            'asesor': client.promotor.asesor,
+            'promotor': client.promotor,
+            'nombre_cliente': client.nombre,
+            'telefono': client.telefono,
+            'correo': client.email,
+            'tipo_credito': client.tramite,
+            'inmueble_identificado': client.inmueble_identificado,
+            'valor_inmueble': client.valor_inmueble,
+            'actividad': client.actividad,
+            'institucion': client.institucion,
+            'pagando_credito_inmb': client.pagando_credito_inmb,
+            'ingreso_mensual': client.ingreso_mensual,
+            'estado_civil': client.estado_civil,
+            'giro_actividad': client.giro_actividad,
+            'actividad_co_acreditado': client.actividad_co_acreditado,
+            'institucion_co_acreditado': client.instituciones_co_acreditado,
+            'pagando_credito_inmb_co_acreditado': client.pagando_credito_inmb_co_acreditado,
+            'ingreso_mensual_co_acreditado': '{:,}'.format(client.ingreso_mensual_co_acreditado),
+            'suma_ingresos': '{:,}'.format(float(client.ingreso_mensual) + float(client.ingreso_mensual_co_acreditado)),
+            'pago_mensual': client.pago_mensual,
+            'cap_endeudamiento': '{:,}'.format(cap_endeudamiento)
+        }
 
-        admin_content += '\n Actividad Co-Acreditado: {}\n Cotiza: {}\n Esta pagando crédito Infonavit o Fovissste: {}'\
-            '\n Ingreso Mensual: {:,}\n'\
-            '\n Suma de Ingresos: {:,}\n Compromisos de Pago Mensual: {:,}\n Capacidad de Endeudamiento: {:,}'.format(
-                client.actividad_co_acreditado,
-                client.instituciones_co_acreditado,
-                client.pagando_credito_inmb_co_acreditado,
-                float(client.ingreso_mensual_co_acreditado),
-                float(client.ingreso_mensual) + float(client.ingreso_mensual_co_acreditado),
-                client.pago_mensual,
-                cap_endeudamiento
-            )
+        html_mail = loader.render_to_string(template_mail, ctx)
 
-        send_mail(admin_subject, admin_content, self.email_sender, [self.email_admin, email_prom, email_as])
+        send_mail(
+            admin_subject, 
+            admin_content, 
+            self.email_sender, 
+            [self.email_admin, email_prom, email_as], 
+            html_message=html_mail
+        )
 
     def email_to_client(self, client):
         client_subject = 'Mi Solución Hipotecaria'
-        client_content = 'Gracias por tu confienza!!'
-        template_mail = 'client_mail.html'
+        client_content = '¡¡Gracias por tu confienza!!'
+
+        if client.promotor.modalidad == 'Asesor':
+            template_mail = 'client_mail_as.html'
+        else:
+            template_mail = 'client_mail_prom.html'
+
         ctx = {
             'despedida': client.promotor.despedida_txt,
             'hsbc': client.hsbc,
