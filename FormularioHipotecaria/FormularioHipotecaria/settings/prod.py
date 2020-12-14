9bc24399198c7c594f2f2d65d3e018c5006d2d31
jeroguzman <jeroguzman@gmail.com>
2020-12-14 18:09:40 +0000

    Cambios en produccion

--- FormularioHipotecaria/FormularioHipotecaria/settings/prod.py
+++ FormularioHipotecaria/FormularioHipotecaria/settings/prod.py
@@ -26,7 +26,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = [str(BASE_DIR / 'staticfiles')]
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 
 # EMAIL SETTINGS
 EMAIL_USE_TLS = True
