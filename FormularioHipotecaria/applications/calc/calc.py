eb144dc9feb583e7ad4512b6e73a695707310abd
oscar jonathan misael lin coronado <ojmlc93@gmail.com>
2020-12-17 19:10:17 -0700

    Misael/Corrección de calculo cuando existe un co acreditado

--- FormularioHipotecaria/applications/calc/calc.py
+++ FormularioHipotecaria/applications/calc/calc.py
@@ -5,24 +5,34 @@ def get_all_estimates(client=None):
     
     if client is not None:          
 
-        if client.ingreso_mensual_co_acreditado is not None:
-            sueldo = float(client.ingreso_mensual) + float(client.ingreso_mensual_co_acreditado)
-        else:
-            sueldo = float(client.ingreso_mensual)
+        if client.ingreso_mensual_co_acreditado is None:
+            client.ingreso_mensual_co_acreditado = 0.0
 
+        sueldo = float(client.ingreso_mensual)
         actividad = client.giro_actividad
         actividades = Actividad.objects.filter(nombre__startswith=actividad)
+        sueldo_co_acreditado = float(client.ingreso_mensual_co_acreditado)
+        actividad_co_acreditado = client.giro_actividad_co_acreditado
         alcances = {}
 
         for activity in actividades:
+            banco = Banco.objects.get(id=activity.banco_id).nombre
             # Cuentas con el ingreso declarado
-            ingreso_actividad = float(sueldo) * activity.castigo
+            # Acreditado
+            ingreso_actividad = sueldo * activity.castigo
+
+            # Co Acreditado
+            if client.actividad_co_acreditado is not None:
+                co_actividad_str = actividad_co_acreditado + '_' + banco.lower()
+                co_actividad = Actividad.objects.get(nombre=co_actividad_str)
+                co_ingreso_actividad = sueldo_co_acreditado * co_actividad.castigo
+                ingreso_actividad = ingreso_actividad + co_ingreso_actividad
+
             cap_endeudamiento = ingreso_actividad * activity.endeudamiento
             cap_pago = cap_endeudamiento - float(client.pago_credito)
 
-            factor_millar =  Banco.objects.get(id=activity.banco_id).factor_millar
+            factor_millar = Banco.objects.get(id=activity.banco_id).factor_millar
             alcance_credito = round((cap_pago / factor_millar) * 1000, 2)
-            banco = Banco.objects.get(id=activity.banco_id).nombre
 
             alcances[banco] = '${:,}'.format(alcance_credito)
 
